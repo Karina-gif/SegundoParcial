cb63349ff6c30c9ecc731c07d001f5f50c5fe8c4
Karina Yazmin Medina Tovar <79298406+Karina-gif@users.noreply.github.com>
2021-03-04 21:20:15 -0600

    agregue libro

--- book-api.js
+++ book-api.js
@@ -15,15 +15,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/book', (req, res) => {
-    app.post('/book', (req, res) => {
-        const book = req.body;
-    
-        // Output the book to the console for debugging
-        console.log(book);
-        books.push(book);
-    
-        res.send('Book is added to the database');
-    });
+    const book = req.body;
+
+    // Output the book to the console for debugging
+    console.log(book);
+    books.push(book);
+
+    res.send('Book is added to the database');
 });
 
-app.listen(port, () => console.log('Hello world app listening on port ${port}!'));
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
